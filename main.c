023a9639e829d77b2535d4ddad230bb6105593f8
bminji0717 <bminji0717@gmail.com>
2026-03-13 00:10:06 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,20 @@
-// 방법2
-// 1의 보수에는 -0이 존재하고, 2의 보수에는 없음을 이용
+// 방법1
 #include <stdio.h>
 
 int main()
 {
-	int a = 0;
-	int b = ~0;
+	int a = 5;	// a에 임의의 값
 
-	int compareA, compareB;
-	for (int i = 0; i < 8; i++)
-	{
-		compareA = (a >> i) & 1;
-		compareB = (b >> i) & 1;
+	int b = ~a;	// b에 a의 모든 비트 반전한 값
 
-		if (compareA != compareB)
-		{
-			printf("two's complement.\n");
-			break;
-		}
-	}
-	if (compareA == compareB)
+	if (b==-a)	// 만약 b(a 비트 반전) 값이 -a와 같다 (-a == ~a)
 	{
 		printf("one's complement.\n");
+	}
+	else		// 비트 반전한 값이 -a가 아니라면
+				// 2의 보수 표현에서 -a == ~a+1
+	{
+		printf("two's complement.\n");
 	}
 
 	return 0;
