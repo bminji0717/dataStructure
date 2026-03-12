11aee3299c969ae94472a749d8c867ffe17bacd5
bminji0717 <bminji0717@gmail.com>
2026-03-13 00:09:28 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+// 방법2
+// 1의 보수에는 -0이 존재하고, 2의 보수에는 없음을 이용
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = ~0;
+
+	int compareA, compareB;
+	for (int i = 0; i < 8; i++)
+	{
+		compareA = (a >> i) & 1;
+		compareB = (b >> i) & 1;
+
+		if (compareA != compareB)
+		{
+			printf("two's complement.\n");
+			break;
+		}
+	}
+	if (compareA == compareB)
+	{
+		printf("one's complement.\n");
+	}
+
+	return 0;
+}
